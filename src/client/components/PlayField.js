17aa51c37cfcabe2f3a50b1935ad37035bfdf9e6
bigpe <AESokolov2@sberbank.ru>
2021-05-31 00:15:17 +0300

    Spawn Figure

--- src/client/components/PlayField.js
+++ src/client/components/PlayField.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Col, Row, Container, Button, ButtonGroup } from 'react-bootstrap';
 import { figures } from '../constants/figurines/figurines';
-import {getRandomInt, getTetrPull} from '../utils/game/createPull';
-import Game from "../pages/Game";
+import { getRandomInt, getTetrPull } from '../utils/game/createPull';
+import { nanoid } from 'nanoid';
+import { rotateFigure } from '../utils/game/rotateFigures';
 
 export function PlayField() {
   const playFieldSize = {
@@ -16,25 +17,40 @@ export function PlayField() {
     Array.from({ length: playFieldSize.width }).map(() => callBack()));
   const generateRandomField = () => generateField(getZeroOrOne);
   const generateEmptyField = () => generateField(getZero);
-
-  const [playFieldMap, setPlayFieldMap] = useState(generateEmptyField());
-  const [allFigures, setAllFigures] = useState(figures.map(figure => {
+  const figuresWithPosition = figures.map(figure => {
     figure.position = 0;
     return figure;
-  }));
+  });
 
-  console.log(allFigures);
+  const [playFieldMap, setPlayFieldMap] = useState(generateEmptyField());
+  const [allFigures, setAllFigures] = useState(figuresWithPosition);
+
+  const figureStartPosition = 4;
+  const pushFigureOnFieldMap = (figure) => {
+    const newPlayFieldMap = generateEmptyField();
+
+    const coords = figure.coords[figure.position];
+    newPlayFieldMap.map((row, rI) => {
+      row.map((col, cI) => {
+        const colIndex = cI + 1;
+        if (colIndex >= figureStartPosition && coords.length > rI) {
+          newPlayFieldMap[rI][cI] = coords[rI][cI - figureStartPosition];
+        }
+      })
+    })
+    setPlayFieldMap([...newPlayFieldMap]);
+  }
 
   const bgFiled = '#90e890';
   const cellSize = 'auto';
 
   const renderField = (fieldMap, color) => fieldMap.map((v, i) =>
-    <Row className={'shadow shadow-sm'} key={`row-${i}`}>
+    <Row className={'shadow shadow-sm'} key={`row-${i}${nanoid()}`}>
       {v.map((h, i) => (
         <Col
           className={`text-center d-flex justify-content-center p-3
         align-items-center border border-light`}
-          key={`col-${i}-${color}`}
+          key={`col-${i}-${color}${nanoid()}`}
           style={{
             height: cellSize,
             width: cellSize,
@@ -48,36 +64,31 @@ export function PlayField() {
   const renderFigure = (figure, onClick = null) => (
     <Container
       className={'align-items-center justify-content-center my-2'}
-      key={`figure-${figure.name}`}
-      onClick={ () => onClick(figure.name) }
+      key={`figure-${figure.name}-${nanoid()}`}
+      onClick={ () => onClick(figure) }
       style={{ display: 'grid', cursor: onClick ? 'pointer' : 'initial' }}
     >
-      {renderField(figure.coords[0], figure.color)}
+      {renderField(figure.coords[figure.position], figure.color)}
     </Container>
   )
 
   const fieldRendered = renderField(playFieldMap, bgFiled);
-  const figureStartPosition = 5;
-  const figuresOnClickHandler = (figureName) => {
-    console.log(figureName);
-    setPlayFieldMap(generateEmptyField);
+  const figuresOnClickHandler = (figure) => {
+    pushFigureOnFieldMap(figure);
   };
   const rotateAllFiguresHandler = () => {
-    allFigures.map(figure => {
-      figure.position = figure.position + 1;
-      return figure;
-    });
-    setAllFigures([...allFigures]);
-    console.log(allFigures);
+    const newFig = allFigures.map(item => rotateFigure(item));
+    setAllFigures(newFig);
   }
   const figuresRendered = allFigures.map(figure => renderFigure(figure, figuresOnClickHandler));
 
   const figuresPullCount = 20;
   const figuresPull = getTetrPull(figuresPullCount);
-  const figuresPullRendered = figuresPull.map(figureName => {
-    const figure = allFigures.find((f) => f.name === figureName);
+  const figuresPullField = figuresPull.map(figureName => {
+    const figure = figures.find((f) => f.name === figureName);
     return renderFigure(figure);
   })
+  const figuresPullRendered = useState(figuresPullField);
 
   return (
     <Container
--- src/client/utils/game/rotateFigures.js
+++ src/client/utils/game/rotateFigures.js
@@ -1,4 +1,4 @@
 export function rotateFigure(figure) {
-  if (figure.position === figure.coords.length) return ({ ...figure, position: 0 });
+  if (figure.position === figure.coords.length - 1) return ({ ...figure, position: 0 });
   return ({ ...figure, position: figure.position + 1 });
 }
